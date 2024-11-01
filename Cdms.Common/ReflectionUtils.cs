299e9b730ecb8af6a9b4c635d21653d96429a0a1
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-11-01 07:15:55 +0000

    suppress sonar message

--- Cdms.Common/ReflectionUtils.cs
+++ Cdms.Common/ReflectionUtils.cs
@@ -1,9 +1,13 @@
-﻿using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace Cdms.Common;
 
 public static class ReflectionUtils
 {
+    [SuppressMessage("SonarLint", "S3011",
+        Justification =
+            "Ignored as this is required to get the internal queue counts from slim message bus, until its exposed")]
     public static TResult GetPrivateFieldValue<TResult>(this object instance, string fieldName)
     {
         var field = instance.GetType()
