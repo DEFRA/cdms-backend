27fab8ff4df8a5683cc65779051340ce8e1c206b
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-11-13 15:54:26 +0000

    fixed compile error

--- CdmsBackent.IntegrationTests/Helpers/LocalBlobService.cs
+++ CdmsBackent.IntegrationTests/Helpers/LocalBlobService.cs
@@ -4,6 +4,16 @@ namespace CdmsBackend.IntegrationTests.Helpers;
 
 public class LocalBlobService(string root) : IBlobService
 {
+    public Task<Status> CheckBlobAsync(int timeout = default, int retries = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Status> CheckBlobAsync(string uri, int timeout = default, int retries = default)
+    {
+        throw new NotImplementedException();
+    }
+
     public IAsyncEnumerable<IBlobItem> GetResourcesAsync(string prefix, CancellationToken cancellationToken)
     {
         return ScanFiles(Path.Combine(root, prefix), cancellationToken);
