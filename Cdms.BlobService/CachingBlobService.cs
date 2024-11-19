533fec4baed4d3b5e4d7ee22cd0505262d47c709
Craig Edmunds <craig.edmunds@gmail.com>
2024-11-19 18:42:45 +0000

    Fixes code style issues

--- Cdms.BlobService/CachingBlobService.cs
+++ Cdms.BlobService/CachingBlobService.cs
@@ -24,27 +24,27 @@ public class CachingBlobService(
     public async IAsyncEnumerable<IBlobItem> GetResourcesAsync(string prefix, CancellationToken cancellationToken)
     {
         var path = Path.GetFullPath($"{options.Value.CachePath}/{prefix}");
-        logger.LogInformation($"Scanning disk {path}");
+        logger.LogInformation("Scanning disk {path}", path);
 
         if (Directory.Exists(path))
         {
-            logger.LogInformation($"Folder {path}exists, looking for files.");  
+            logger.LogInformation("Folder {path} exists, looking for files.", path);  
            foreach (string f in Directory.GetFiles(path, "*.json", SearchOption.AllDirectories))
            {
                var relativePath = Path.GetRelativePath($"{Directory.GetCurrentDirectory()}/{options.Value.CachePath}", f);
-                logger.LogInformation($"Found file {relativePath}");
+                logger.LogInformation("Found file {relativePath}", relativePath);
                 yield return new SynchroniserBlobItem() { Name = relativePath };
             }           
         }
         else{
-            logger.LogWarning($"Cannot scan folder {path} as it doesn't exist.");    
+            logger.LogWarning("Cannot scan folder {path} as it doesn't exist.", path);    
         }
     }
 
     public Task<string> GetResource(IBlobItem item, CancellationToken cancellationToken)
     {
         var filePath = $"{options.Value.CachePath}/{item.Name}";
-        logger.LogInformation($"GetResource {filePath}");
-        return Task.Run(() => File.ReadAllText(filePath));  //blobService.GetResource(item, cancellationToken);
+        logger.LogInformation("GetResource {filePath}", filePath);
+        return Task.Run(() => File.ReadAllText(filePath));
     }
 }
