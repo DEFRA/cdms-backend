6d4fcc4ec71ee0dcafeadd107a707deeb3d16d91
t11omas <t11omas1983@live.co.uk>
2024-11-26 21:24:35 +0000

    changed mongo db context to scoped because it stores the state of the active transaction (#42)
    Co-authored-by: Thomas Anderson <thomas.anderson@esynergy.co.uk>

--- CdmsBackend/Program.cs
+++ CdmsBackend/Program.cs
@@ -1,38 +1,36 @@
+using Cdms.Analytics;
+using Cdms.Backend.Data.Healthcheck;
+using Cdms.BlobService;
+using Cdms.Business.Extensions;
+using Cdms.Common.Extensions;
+using Cdms.Consumers.Extensions;
+using Cdms.Emf;
+using Cdms.SyncJob.Extensions;
+using CdmsBackend.BackgroundTaskQueue;
+using CdmsBackend.Config;
 using CdmsBackend.Endpoints;
+using CdmsBackend.JsonApi;
+using CdmsBackend.Mediatr;
 using CdmsBackend.Utils;
 using CdmsBackend.Utils.Http;
 using CdmsBackend.Utils.Logging;
 using FluentValidation;
-using Serilog;
-using Serilog.Core;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using System.Text.Json.Serialization;
-using Cdms.Analytics;
-using Cdms.Business.Extensions;
-using Cdms.BlobService;
-using Cdms.Backend.Data.Healthcheck;
-using Cdms.Business;
-using Cdms.Consumers.Extensions;
-using Cdms.Emf;
-using Cdms.SyncJob.Extensions;
-using CdmsBackend.Config;
 using HealthChecks.UI.Client;
 using JsonApiDotNetCore.Configuration;
 using JsonApiDotNetCore.MongoDb.Configuration;
+using JsonApiDotNetCore.MongoDb.Repositories;
+using JsonApiDotNetCore.Repositories;
+using JsonApiDotNetCore.Serialization.Response;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
-using JsonApiDotNetCore.MongoDb.Repositories;
-using JsonApiDotNetCore.Repositories;
-using CdmsBackend.BackgroundTaskQueue;
-using CdmsBackend.Mediatr;
-using CdmsBackend.JsonApi;
-using Cdms.Common.Extensions;
-using Humanizer;
-using JsonApiDotNetCore.Serialization.Response;
+using Serilog;
+using Serilog.Core;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Json.Serialization;
 using Environment = System.Environment;
 
 //-------- Configure the WebApplication builder------------------//
@@ -44,181 +42,182 @@ await app.RunAsync();
 [ExcludeFromCodeCoverage]
 static WebApplication CreateWebApplication(string[] args)
 {
-    var builder = WebApplication.CreateBuilder(args);
+	var builder = WebApplication.CreateBuilder(args);
 
-    ConfigureWebApplication(builder);
+	ConfigureWebApplication(builder);
 
-    var app = BuildWebApplication(builder);
+	var app = BuildWebApplication(builder);
 
-    return app;
+	return app;
 }
 
 [ExcludeFromCodeCoverage]
 static void ConfigureWebApplication(WebApplicationBuilder builder)
 {
-    builder.Services.ConfigureHttpJsonOptions(options =>
-    {
-        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
-    
-    builder.Services.AddSingleton<ICdmsMediator, CdmsMediator>();
-    builder.Services.AddSyncJob();
-    builder.Services.AddHostedService<QueueHostedService>();
-    builder.Services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
-    builder.Configuration.AddEnvironmentVariables();
+	builder.Services.ConfigureHttpJsonOptions(options =>
+	{
+		options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+	});
 
-    var logger = ConfigureLogging(builder);
+	builder.Services.AddSingleton<ICdmsMediator, CdmsMediator>();
+	builder.Services.AddSyncJob();
+	builder.Services.AddHostedService<QueueHostedService>();
+	builder.Services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+	builder.Configuration.AddEnvironmentVariables();
 
-    if (!builder.Configuration.GetValue<bool>("DisableLoadIniFile"))
-    {
-        builder.Configuration.AddIniFile("Properties/local.env", true)
-            .AddIniFile($"Properties/local.{builder.Environment.EnvironmentName}.env", true);
-    }
-    
-    builder.Services.CdmsAddOptions<ApiOptions, ApiOptions.Validator>(builder.Configuration, ApiOptions.SectionName)
-        .PostConfigure(options => builder.Configuration.Bind(options));
-    
+	var logger = ConfigureLogging(builder);
 
-    // Load certificates into Trust Store - Note must happen before Mongo and Http client connections
-    builder.Services.AddCustomTrustStore(logger);
+	if (!builder.Configuration.GetValue<bool>("DisableLoadIniFile"))
+	{
+		builder.Configuration.AddIniFile("Properties/local.env", true)
+			.AddIniFile($"Properties/local.{builder.Environment.EnvironmentName}.env", true);
+	}
 
-    builder.Services.AddBusinessServices(builder.Configuration);
-    builder.Services.AddConsumers(builder.Configuration);
+	builder.Services.CdmsAddOptions<ApiOptions, ApiOptions.Validator>(builder.Configuration, ApiOptions.SectionName)
+		.PostConfigure(options => builder.Configuration.Bind(options));
 
-    ConfigureEndpoints(builder);
 
-    builder.Services.AddHttpClient();
+	// Load certificates into Trust Store - Note must happen before Mongo and Http client connections
+	builder.Services.AddCustomTrustStore(logger);
 
-    // calls outside the platform should be done using the named 'proxy' http client.
-    builder.Services.AddHttpProxyClient();
+	builder.Services.AddBusinessServices(builder.Configuration);
+	builder.Services.AddConsumers(builder.Configuration);
 
-    builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+	ConfigureEndpoints(builder);
 
-    // This added Open Telemetry, and export to look at metrics locally.
-    // The Aspire dashboard can be used to view these metrics  :
-    // docker run --rm -it -p 18888:18888 -p 4317:18889 -d --name aspire-dashboard -e DOTNET_DASHBOARD_UNSECURED_ALLOW_ANONYMOUS='true' mcr.microsoft.com/dotnet/nightly/aspire-dashboard:8.0.0-preview.6
+	builder.Services.AddHttpClient();
 
-    if (builder.Environment.IsDevelopment())
-    {
-        builder.Services.AddOpenTelemetry()
-            .WithMetrics(metrics =>
-            {
-                metrics.AddRuntimeInstrumentation()
-                    .AddMeter(
-                        "Microsoft.AspNetCore.Hosting",
-                        "Microsoft.AspNetCore.Server.Kestrel",
-                        "System.Net.Http",
-                        "Cdms");
-            });
+	// calls outside the platform should be done using the named 'proxy' http client.
+	builder.Services.AddHttpProxyClient();
 
-        builder.Services.AddOpenTelemetry()
-            .WithTracing(tracing =>
-            {
-                tracing.AddAspNetCoreInstrumentation()
-                    .AddHttpClientInstrumentation();
-            });
+	builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-        builder.Services.AddOpenTelemetry().UseOtlpExporter();
-    }
-    else
-    {
-        // Assumes we're in CDP... - this doesn't exist
-        // builder.Services.AddOpenTelemetry().UseEmfExporter()
-    }
+	// This added Open Telemetry, and export to look at metrics locally.
+	// The Aspire dashboard can be used to view these metrics  :
+	// docker run --rm -it -p 18888:18888 -p 4317:18889 -d --name aspire-dashboard -e DOTNET_DASHBOARD_UNSECURED_ALLOW_ANONYMOUS='true' mcr.microsoft.com/dotnet/nightly/aspire-dashboard:8.0.0-preview.6
 
-    static void ConfigureJsonApiOptions(JsonApiOptions options)
-    {
-        options.Namespace = "api";
-        options.UseRelativeLinks = true;
-        options.IncludeTotalResourceCount = true;
-        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        options.ClientIdGeneration = ClientIdGenerationMode.Allowed;
+	if (builder.Environment.IsDevelopment())
+	{
+		builder.Services.AddOpenTelemetry()
+			.WithMetrics(metrics =>
+			{
+				metrics.AddRuntimeInstrumentation()
+					.AddMeter(
+						"Microsoft.AspNetCore.Hosting",
+						"Microsoft.AspNetCore.Server.Kestrel",
+						"System.Net.Http",
+						"Cdms");
+			});
+
+		builder.Services.AddOpenTelemetry()
+			.WithTracing(tracing =>
+			{
+				tracing.AddAspNetCoreInstrumentation()
+					.AddHttpClientInstrumentation();
+			});
+
+		builder.Services.AddOpenTelemetry().UseOtlpExporter();
+	}
+	else
+	{
+		// Assumes we're in CDP... - this doesn't exist
+		// builder.Services.AddOpenTelemetry().UseEmfExporter()
+	}
+
+	static void ConfigureJsonApiOptions(JsonApiOptions options)
+	{
+		options.Namespace = "api";
+		options.UseRelativeLinks = true;
+		options.IncludeTotalResourceCount = true;
+		options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+		options.ClientIdGeneration = ClientIdGenerationMode.Allowed;
 #if DEBUG
-        options.IncludeExceptionStackTraceInErrors = true;
-        options.IncludeRequestBodyInErrors = true;
-        options.SerializerOptions.WriteIndented = true;
+		options.IncludeExceptionStackTraceInErrors = true;
+		options.IncludeRequestBodyInErrors = true;
+		options.SerializerOptions.WriteIndented = true;
 #endif
-    }
+	}
 
-    builder.Services.AddJsonApi(ConfigureJsonApiOptions,
-        discovery => discovery.AddAssembly(Assembly.Load("Cdms.Model")));
+	builder.Services.AddJsonApi(ConfigureJsonApiOptions,
+		discovery => discovery.AddAssembly(Assembly.Load("Cdms.Model")));
 
-    builder.Services.AddJsonApiMongoDb();
-    builder.Services.AddScoped<IResponseModelAdapter, CdmsResponseModelAdapter>();
-    builder.Services.AddScoped(typeof(IResourceReadRepository<,>), typeof(MongoRepository<,>));
-    builder.Services.AddScoped(typeof(IResourceWriteRepository<,>), typeof(MongoRepository<,>));
-    builder.Services.AddScoped(typeof(IResourceRepository<,>), typeof(MongoRepository<,>));
-    builder.Services.AddSingleton<IMatchingAggregationService,MatchingAggregationService>();
+	builder.Services.AddJsonApiMongoDb();
+	builder.Services.AddScoped<IResponseModelAdapter, CdmsResponseModelAdapter>();
+	builder.Services.AddScoped(typeof(IResourceReadRepository<,>), typeof(MongoRepository<,>));
+	builder.Services.AddScoped(typeof(IResourceWriteRepository<,>), typeof(MongoRepository<,>));
+	builder.Services.AddScoped(typeof(IResourceRepository<,>), typeof(MongoRepository<,>));
+	builder.Services.AddScoped<IMatchingAggregationService, MatchingAggregationService>();
 }
 
 [ExcludeFromCodeCoverage]
 static Logger ConfigureLogging(WebApplicationBuilder builder)
 {
-    builder.Logging.ClearProviders();
-    var logBuilder = new LoggerConfiguration()
-        .ReadFrom.Configuration(builder.Configuration)
-        .Enrich.With<LogLevelMapper>()
-        .Enrich.WithProperty("service.version", Environment.GetEnvironmentVariable("SERVICE_VERSION"));
+	builder.Logging.ClearProviders();
+	var logBuilder = new LoggerConfiguration()
+		.ReadFrom.Configuration(builder.Configuration)
+		.Enrich.With<LogLevelMapper>()
+		.Enrich.WithProperty("service.version", Environment.GetEnvironmentVariable("SERVICE_VERSION"));
 
-    if (builder.Environment.IsDevelopment())
-    {
-        logBuilder
-            .WriteTo.OpenTelemetry(options =>
-            {
-                options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-                options.ResourceAttributes.Add("service.name", "cdmsbackend");
-            });
-    }
+	if (builder.Environment.IsDevelopment())
+	{
+		logBuilder
+			.WriteTo.OpenTelemetry(options =>
+			{
+				options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+				options.ResourceAttributes.Add("service.name", "cdmsbackend");
+			});
+	}
 
-    var logger = logBuilder.CreateLogger();
-    builder.Logging.AddSerilog(logger);
-    logger.Information("Starting application");
-    return logger;
+	var logger = logBuilder.CreateLogger();
+	builder.Logging.AddSerilog(logger);
+	logger.Information("Starting application");
+	return logger;
 }
 
 [ExcludeFromCodeCoverage]
 static void ConfigureEndpoints(WebApplicationBuilder builder)
 {
-    builder.Services.AddHealthChecks()
-        .AddAzureBlobStorage(sp => sp.GetService<IBlobServiceClientFactory>()!.CreateBlobServiceClient(5, 1), timeout: TimeSpan.FromSeconds(15))
-        .AddMongoDb(timeout: TimeSpan.FromSeconds(15));
+	builder.Services.AddHealthChecks()
+		.AddAzureBlobStorage(sp => sp.GetService<IBlobServiceClientFactory>()!.CreateBlobServiceClient(5, 1), timeout: TimeSpan.FromSeconds(15))
+		.AddMongoDb(timeout: TimeSpan.FromSeconds(15));
 }
 
 [ExcludeFromCodeCoverage]
 static WebApplication BuildWebApplication(WebApplicationBuilder builder)
 {
-    var app = builder.Build();
+	var app = builder.Build();
 
-    app.UseEmfExporter();
-    app.UseJsonApi();
-    app.MapControllers();
-    
-    var dotnetHealthEndpoint = "/health-dotnet";
-    app.MapGet("/health", GetStatus).AllowAnonymous();
-    app.MapHealthChecks(dotnetHealthEndpoint,
-        new HealthCheckOptions()
-        {
-            Predicate = _ => true, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-        });
-    
-    var options = app.Services.GetRequiredService<IOptions<ApiOptions>>();
-    app.UseSyncEndpoints(options);
-    app.UseManagementEndpoints(options);
-    app.UseDiagnosticEndpoints(options);
-    app.UseAnalyticsEndpoints();
-    
-    return app;
+	app.UseEmfExporter();
+	app.UseJsonApi();
+	app.MapControllers();
+
+	var dotnetHealthEndpoint = "/health-dotnet";
+	app.MapGet("/health", GetStatus).AllowAnonymous();
+	app.MapHealthChecks(dotnetHealthEndpoint,
+		new HealthCheckOptions()
+		{
+			Predicate = _ => true,
+			ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+		});
+
+	var options = app.Services.GetRequiredService<IOptions<ApiOptions>>();
+	app.UseSyncEndpoints(options);
+	app.UseManagementEndpoints(options);
+	app.UseDiagnosticEndpoints(options);
+	app.UseAnalyticsEndpoints();
+
+	return app;
 }
 
 static IResult GetStatus()
 {
-    return Results.Ok();
+	return Results.Ok();
 }
 
 //Here to it can be referenced by integration tests
 public partial class Program
 {
-    protected Program()
-    {
-    }
+	protected Program()
+	{
+	}
 }
