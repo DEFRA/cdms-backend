96dde9e832c50c7c0ee2681362e6cbc2d314c8c2
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-11-27 10:15:33 +0000

    Updated management service to include the indexes on the collection, and updated the index service to log errors

--- Cdms.Backend.Data/Mongo/MongoIndexService.cs
+++ Cdms.Backend.Data/Mongo/MongoIndexService.cs
@@ -1,11 +1,12 @@
 using Cdms.Model;
 using Cdms.Model.Ipaffs;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace Cdms.Backend.Data.Mongo;
 
-public class MongoIndexService(IMongoDatabase database) : IHostedService
+public class MongoIndexService(IMongoDatabase database, ILogger<MongoIndexService> logger) : IHostedService
 {
     public Task StartAsync(CancellationToken cancellationToken)
     {
@@ -23,14 +24,22 @@ public class MongoIndexService(IMongoDatabase database) : IHostedService
         return Task.CompletedTask;
     }
 
-    private Task CreateIndex<T>(string name, IndexKeysDefinition<T> keys, CancellationToken cancellationToken)
+    private async Task CreateIndex<T>(string name, IndexKeysDefinition<T> keys, CancellationToken cancellationToken)
     {
-        var indexModel = new CreateIndexModel<T>(keys,
-            new CreateIndexOptions()
-            {
-                Name = name,
-                Background = true,
-            });
-        return database.GetCollection<T>(typeof(T).Name).Indexes.CreateOneAsync(indexModel, cancellationToken: cancellationToken);
+        try
+        {
+            var indexModel = new CreateIndexModel<T>(keys,
+                new CreateIndexOptions()
+                {
+                    Name = name,
+                    Background = true,
+                });
+            await database.GetCollection<T>(typeof(T).Name).Indexes.CreateOneAsync(indexModel, cancellationToken: cancellationToken);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Failed to Create index {Name} on {Collection}", name, typeof(T).Name);
+        }
+       
     }
 }
