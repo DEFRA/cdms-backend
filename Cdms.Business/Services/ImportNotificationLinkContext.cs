da84298028d712d817fc19007e0163156295df46
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-12-02 07:27:55 +0000

    added logging around the link service

--- Cdms.Business/Services/ImportNotificationLinkContext.cs
+++ Cdms.Business/Services/ImportNotificationLinkContext.cs
@@ -2,4 +2,10 @@ using Cdms.Model.Ipaffs;
 
 namespace Cdms.Business.Services;
 
-public record ImportNotificationLinkContext(ImportNotification ReceivedImportNotification, ImportNotification? ExistingImportNotification) : LinkContext;
+public record ImportNotificationLinkContext(ImportNotification ReceivedImportNotification, ImportNotification? ExistingImportNotification) : LinkContext
+{
+    public override string GetIdentifiers()
+    {
+        return ReceivedImportNotification._MatchReference;
+    }
+}
