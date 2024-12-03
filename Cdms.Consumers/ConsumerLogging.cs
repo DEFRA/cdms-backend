12a3ae964b98428adc0d2fe23762cb46a6f06399
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-12-03 05:59:19 +0000

    Fixed logging message

--- Cdms.Consumers/ConsumerLogging.cs
+++ Cdms.Consumers/ConsumerLogging.cs
@@ -4,7 +4,7 @@ namespace Cdms.Consumers;
 
 internal static partial class ConsumerLogging
 {
-    [LoggerMessage(Level = LogLevel.Information, Message = "Message skipped - {JobId} - {MessageId} - {Consumer} - {Identifier}")]
+    [LoggerMessage(Level = LogLevel.Information, Message = "Consumer Started - {JobId} - {MessageId} - {Consumer} - {Identifier}")]
     internal static partial void ConsumerStarted(this ILogger logger, string jobId, string messageId, string consumer, string identifier);
 
     [LoggerMessage(Level = LogLevel.Information, Message = "Message skipped - {JobId} - {MessageId} - {Consumer} - {Identifier}")]
